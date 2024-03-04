0a844ac56d22b6bcca1af7cae909876c71606613
Igor Serdyukov <sis000@mail.ru>
2024-03-04 13:18:18 +0300

    [upd] WatchDocumentService implemented

--- src/Models/Revit2020/WatchDocumentService.cs
+++ src/Models/Revit2020/WatchDocumentService.cs
@@ -1,24 +1,47 @@
 ﻿namespace WS.Models.Revit2020
 {
+    using Autodesk.Revit.ApplicationServices;
+    using Autodesk.Revit.DB;
+    using Autodesk.Revit.DB.Events;
     using Contracts;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class WatchDocumentService : IWatchDocument
     {
+        private Application _application;
+
+        public WatchDocumentService(Application application)
+        {
+            _application = application;
+        }
+
         public event Action<bool> Activated;
 
         public void Start()
         {
-            throw new NotImplementedException();
+            _application.DocumentOpened +=
+                On_Application_DocumentOpened;
+
+            // may be a wrong to detect if a doc is active
+            var anyDocIsActive = _application
+                .Documents
+                .Cast<Document>()
+                .Any(d => d is not null);
+
+            Activated?.Invoke(anyDocIsActive);
         }
 
         public void Stop()
         {
             throw new NotImplementedException();
+        }
+
+        private void On_Application_DocumentOpened(
+            object sender,
+            DocumentOpenedEventArgs e)
+        {
+            Activated?.Invoke(e.Document is not null);
         }
     }
 }
